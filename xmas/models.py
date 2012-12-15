3b9b5340c15fc8af294bb0b8fe3668ad3f87950f
James Cleveland <jamescleveland@gmail.com>
2012-12-15 00:34:01 +0000

    ohh I was sending the email all the time

--- xmas/models.py
+++ xmas/models.py
@@ -20,21 +20,22 @@ class Card(models.Model):
         })
 
     def send(self):
-        send_mail('Oh my! A Christmas card? Wow!',
-            ('It appears that I\'ve sent you a Christmas card. Take a deep '
-            + 'breath, let that sink in for a moment, then click here to have '
-            + 'a looksie: \n\n'
-            + 'http://blackflags.co.uk{}'
-            + '\n\n'
-            + 'Toodle pip!'
-            ).format(
-                self.get_absolute_url()
-            ),
-            'James Cleveland <jc@blackflags.co.uk>',
-            [self.email],
-            fail_silently=False
-        )
         self.sent = True
+        if not self.sent:
+            send_mail('Oh my! A Christmas card? Wow!',
+                ('It appears that I\'ve sent you a Christmas card. Take a deep '
+                + 'breath, let that sink in for a moment, then click here to have '
+                + 'a looksie: \n\n'
+                + 'http://blackflags.co.uk{}'
+                + '\n\n'
+                + 'Toodle pip!'
+                ).format(
+                    self.get_absolute_url()
+                ),
+                'James Cleveland <jc@blackflags.co.uk>',
+                [self.email],
+                fail_silently=False
+            )
 
     def __unicode__(self):
         return u'{recipient} ({uuid})'.format(**self.__dict__)
